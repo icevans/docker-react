43c348e1f0758653b440135e546518996d75cb2b
Ian Evans <icevans@icloud.com>
2020-03-23 16:33:39 -0600

    Update app text to improve manners

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn react
+          Please, learn react
         </a>
       </header>
     </div>
